3a20e79bd48f02dfea10bfbe033af1d4ceadcc0b
Nilson Ivano <nilson.ivano@gmail.com>
2016-07-06 09:23:23 -0300

    Kadira e HotJar

--- client/main.js
+++ client/main.js
@@ -1,7 +1,27 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+
+Meteor.startup(function(){
+    $(window).scroll(function(){
+        windowPosition = $(window).scrollTop() + $(window).height();
+        perc = windowPosition / $(document).height() * 100;
+        if (perc > 25 && perc < 50){
+            Session.set("scrollPercentage", 25);
+        }
+        if (perc > 50 && perc < 75){
+            Session.set("scrollPercentage", 50);
+        }
+        if (perc > 75 && perc < 99){
+            Session.set("scrollPercentage", 75);
+        }
+        if (perc > 99){
+            Session.set("scrollPercentage", 100);
+        }
+    });
+});
 
 Template.indicaPsicoHome.onRendered(function(){
     $('.parallax').parallax();
@@ -12,6 +32,14 @@ Template.indicaPsicoHome.onRendered(function(){
     });
     $('#contatoForm').hide();
     $('.scrollspy').scrollSpy();
+    Tracker.autorun(function(){
+        perc = Session.get("scrollPercentage");
+        ga('send', 'event', {
+            eventCategory: 'SiteTracking',
+            eventAction: 'userScrollPage',
+            eventValue: perc
+        });
+    });
 });
 
 Template.indicaForm.helpers({
@@ -46,6 +74,12 @@ Template.indicaForm.events({
   'click #contatoNao': function(){
     $('#contatoForm').hide();
   },
+  'click #buttonIndicaPsico': function(){
+      ga('send', 'event', {
+          eventCategory: 'SiteTracking',
+          eventAction: 'buttonIndicaProfissionaisClick'
+      });
+  },
   'click #requisitaPsico': function(event){
     event.preventDefault();
     var name = $('#name').val();
@@ -56,6 +90,7 @@ Template.indicaForm.events({
     var atendimentoType = $('#atendimentoType').val();
     var especializacaoType = $('#especializacaoType').val();
     var abordagemType = $('#abordagemType').val();
+    var checkConvenio = $('input[name="convenioGroup"]:checked').val();
     var haveConvenio = $('input[name="convenioGroup"]:checked').val();
     if(haveConvenio == "convenioSim"){
       haveConvenio = true;
@@ -63,6 +98,7 @@ Template.indicaForm.events({
       haveConvenio = false;
     }
     var nomeConvenio = $('#nomeConvenio').val();
+    var checkContatar = $('input[name="contato"]:checked').val();
     var haveContatar = $('input[name="contato"]:checked').val();
     if(haveContatar == "contatoSim"){
       haveContatar = true;
@@ -85,81 +121,102 @@ Template.indicaForm.events({
         periodoList.push(periodo);
       }
     }
-    Meteor.call('insertRequestPsico',name, email, phone, address, valores, atendimentoType, especializacaoType, abordagemType,
-    haveConvenio, nomeConvenio, haveContatar, contatoList, periodoList, function(err, results){
-          if(err){
-            console.log(err)
-          } else{
-            if(haveConvenio == true){
-              haveConvenio = "Sim"
-            } else{
-              haveConvenio = "Não"
-            }
-            if(haveContatar == true){
-              haveContatar = "Sim"
-            } else{
-              haveContatar = "Não"
-            }
-            //Sending emails
-            var html =
-                '<div>' + 'Olá ' + name + ', sua proposta foi enviada ao IndicaPsico' +
-                ' Pedimos agora que aguarde o nosso contato com a sugestão de psicólogos conforme sua necessidade. Abaixo você pode revisar as informações que você enviou:' + '</div>' +
-                '</br>' +
-                '<div>' + '<b>Nome: </b>' + name + '</div>' +
-                '<div>' + '<b>Email: </b>' + email + '</div>' +
-                '<div>' + '<b>Telefone: </b>' + phone + '<div>' +
-                '<div>' + '<b>Endereço: </b>' + address + '<div>' +
-                '<div>' + '<b>Valor: </b>' + valores + '<div>' +
-                '<div>' + '<b>Tipo de atendimento: </b>' + atendimentoType + '<div>' +
-                '<div>' + '<b>Tipo de especialização: </b>' + especializacaoType + '<div>' +
-                '<div>' + '<b>Tipo de abordagem: </b>' + abordagemType + '<div>' +
-                '<div>' + '<b>Convênio: </b>' + haveConvenio + '<div>' +
-                '<div>' + '<b>Nome do Convênio: </b>' + nomeConvenio + '<div>' +
-                '<div>' + '<b>Contatar: </b>' + haveContatar + '<div>' +
-                '<div>' + '<b>Tipo de Contato: </b>' + contatoList + '<div>' +
-                '<div>' + '<b>Período de Contato: </b>' + periodoList + '<div>' +
-                '</br>' +
-                '<div>' + 'Obrigado por utilizar o IndicaPsico (www.indicapsico.com.br)' + '</div>';
-            Meteor.call('sendEmail',
-                email,
-                'contato@indicapsico.com.br',
-                'Sua requisição foi enviada com sucesso para o IndicaPsico',
-                html,
-                function(err, results){
-                  if(err){
-                    console.log(err)
-                  }
-                });
-          }
-            var htmlIndicaPsico =
-              '<div>' + 'Nova requisição no IndicaPsico' + '</div>' +
-              '</br>' +
-              '<div>' + '<b>Nome: </b>' + name + '</div>' +
-              '<div>' + '<b>Email: </b>' + email + '</div>' +
-              '<div>' + '<b>Telefone: </b>' + phone + '<div>' +
-              '<div>' + '<b>Endereço: </b>' + address + '<div>' +
-              '<div>' + '<b>Valor: </b>' + valores + '<div>' +
-              '<div>' + '<b>Tipo de atendimento: </b>' + atendimentoType + '<div>' +
-              '<div>' + '<b>Tipo de especialização: </b>' + especializacaoType + '<div>' +
-              '<div>' + '<b>Tipo de abordagem: </b>' + abordagemType + '<div>' +
-              '<div>' + '<b>Convênio: </b>' + haveConvenio + '<div>' +
-              '<div>' + '<b>Nome do Convênio: </b>' + nomeConvenio + '<div>' +
-              '<div>' + '<b>Contatar: </b>' + haveContatar + '<div>' +
-              '<div>' + '<b>Tipo de Contato: </b>' + contatoList + '<div>' +
-              '<div>' + '<b>Período de Contato: </b>' + periodoList + '<div>';
-            Meteor.call('sendEmail',
-              'contato@indicapsico.com.br',
-              'contato@indicapsico.com.br',
-              'Nova requisição IndicaPsico',
-              htmlIndicaPsico,
-              function(err, results){
+    if(name && email && phone && address && valores && atendimentoType && checkConvenio && checkContatar){
+        Meteor.call('insertRequestPsico',name, email, phone, address, valores, atendimentoType, especializacaoType, abordagemType,
+            haveConvenio, nomeConvenio, haveContatar, contatoList, periodoList, function(err, results){
                 if(err){
-                  Materialize.toast('Sua requisição não foi enviada. Favor insira todas as informações obrigatórias.', 10000);
+                    console.log(err)
+                } else{
+                    if(haveConvenio == true){
+                        haveConvenio = "Sim"
+                    } else{
+                        haveConvenio = "Não"
+                    }
+                    if(haveContatar == true){
+                        haveContatar = "Sim"
+                    } else{
+                        haveContatar = "Não"
+                    }
+                    //Sending emails
+                    var html =
+                        '<div>' + 'Olá ' + name + ', sua proposta foi enviada ao IndicaPsico' +
+                        ' Pedimos agora que aguarde o nosso contato com a sugestão de psicólogos conforme sua necessidade. Abaixo você pode revisar as informações que você enviou:' + '</div>' +
+                        '</br>' +
+                        '<div>' + '<b>Nome: </b>' + name + '</div>' +
+                        '<div>' + '<b>Email: </b>' + email + '</div>' +
+                        '<div>' + '<b>Telefone: </b>' + phone + '<div>' +
+                        '<div>' + '<b>Endereço: </b>' + address + '<div>' +
+                        '<div>' + '<b>Valor: </b>' + valores + '<div>' +
+                        '<div>' + '<b>Tipo de atendimento: </b>' + atendimentoType + '<div>' +
+                        '<div>' + '<b>Tipo de especialização: </b>' + especializacaoType + '<div>' +
+                        '<div>' + '<b>Tipo de abordagem: </b>' + abordagemType + '<div>' +
+                        '<div>' + '<b>Convênio: </b>' + haveConvenio + '<div>' +
+                        '<div>' + '<b>Nome do Convênio: </b>' + nomeConvenio + '<div>' +
+                        '<div>' + '<b>Contatar: </b>' + haveContatar + '<div>' +
+                        '<div>' + '<b>Tipo de Contato: </b>' + contatoList + '<div>' +
+                        '<div>' + '<b>Período de Contato: </b>' + periodoList + '<div>' +
+                        '</br>' +
+                        '<div>' + 'Obrigado por utilizar o IndicaPsico (www.indicapsico.com.br)' + '</div>';
+                    Meteor.call('sendEmail',
+                        email,
+                        'contato@indicapsico.com.br',
+                        'Sua requisição foi enviada com sucesso para o IndicaPsico',
+                        html,
+                        function(err, results){
+                            if(err){
+                                console.log(err)
+                            }
+                        });
                 }
-              });
-          Materialize.toast('Sua requisição foi enviada com sucesso. Em breve retornaremos com o psicólogo mais indicado para suas necessidades.', 10000);
-          $("#indicaForm").find('input:text, input:password, input:file, select, textarea').val('');
-          $("#indicaForm").find('input:radio, input:checkbox').removeAttr('checked').removeAttr('selected');
+                var htmlIndicaPsico =
+                    '<div>' + 'Nova requisição no IndicaPsico' + '</div>' +
+                    '</br>' +
+                    '<div>' + '<b>Nome: </b>' + name + '</div>' +
+                    '<div>' + '<b>Email: </b>' + email + '</div>' +
+                    '<div>' + '<b>Telefone: </b>' + phone + '<div>' +
+                    '<div>' + '<b>Endereço: </b>' + address + '<div>' +
+                    '<div>' + '<b>Valor: </b>' + valores + '<div>' +
+                    '<div>' + '<b>Tipo de atendimento: </b>' + atendimentoType + '<div>' +
+                    '<div>' + '<b>Tipo de especialização: </b>' + especializacaoType + '<div>' +
+                    '<div>' + '<b>Tipo de abordagem: </b>' + abordagemType + '<div>' +
+                    '<div>' + '<b>Convênio: </b>' + haveConvenio + '<div>' +
+                    '<div>' + '<b>Nome do Convênio: </b>' + nomeConvenio + '<div>' +
+                    '<div>' + '<b>Contatar: </b>' + haveContatar + '<div>' +
+                    '<div>' + '<b>Tipo de Contato: </b>' + contatoList + '<div>' +
+                    '<div>' + '<b>Período de Contato: </b>' + periodoList + '<div>';
+                Meteor.call('sendEmail',
+                    'contato@indicapsico.com.br',
+                    'contato@indicapsico.com.br',
+                    'Nova requisição IndicaPsico',
+                    htmlIndicaPsico,
+                    function(err, results){
+                        if(err){
+                            Materialize.toast('Sua requisição não foi enviada. Ocorreu algum erro no envio.', 10000);
+                            ga('send', 'event', {
+                                eventCategory: 'SiteTracking',
+                                eventAction: 'buttonRequestPsicoClick',
+                                eventLabel: 'Erro'
+                            });
+                        }
+                    });
+                $("#modalRequestPsicoSuccess").openModal();
+                //Materialize.toast('Sua requisição foi enviada com sucesso. Em breve retornaremos com o psicólogo mais indicado para suas necessidades.', 10000);
+                ga('send', 'event', {
+                    eventCategory: 'SiteTracking',
+                    eventAction: 'buttonRequestPsicoClick',
+                    eventLabel: 'Envio com sucesso'
+                });
+                $("#indicaForm").find('input:text, input:password, input:file, select, textarea').val('');
+                $("#indicaForm").find('input:radio, input:checkbox').removeAttr('checked').removeAttr('selected');
+            });
+    } else{
+        Materialize.toast('Por favor preencha todos os campos obrigatórios.', 5000);
+        ga('send', 'event', {
+            eventCategory: 'SiteTracking',
+            eventAction: 'buttonRequestPsicoClick',
+            eventLabel: 'Falta Campos Obrigatorios'
         });
+    }
+
   }
 });
---
+++ server/kadira.js
@@ -0,0 +1,3 @@
+Meteor.startup(function() {
+    Kadira.connect('LSju8w3dkWoHyufya', 'fbfba865-98b4-4c97-a968-f69bdf2055d2');
+});
